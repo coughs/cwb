5da1855013b88f13544256fdb52fff1680471c32
CongWB <congwb@cong.com.cn>
2018-10-18 20:10:21 +0800

    1,2,3

---
+++ Study/coduck/20181018/1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int x,y;
+	cin>>x>>y;
+	cout<<"等腰三角形的面积是"<<x*y/2<<"平方米";
+	return 0;
+}
---
+++ Study/coduck/20181018/3.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main2(){
+	cout<<"hi"<<endl;
+	cout<<"   |"<<endl;
+	cout<<"  /|\\"<<endl;
+	cout<<"  |||"<<endl;
+	cout<<"  |||"<<endl;
+	cout<<"  |||"<<endl;
+	cout<<" /|||\\ "<<endl;
+	cout<<" |||||"<<endl;
+	cout<<" |||||"<<endl;
+	cout<<" |-|-|"<<endl;
+	cout<<" * * *"<<endl;
+	cout<<"* * * *"<<endl;
+	cout<<"*******"<<endl;
+	cout<<""<<endl;
+	cout<<""<<endl;
+	return 0;
+}
+
+int main(){
+	//cout<<"Hello, world!"<<endl;
+	//main2();
+	int a,b;
+	a=3;
+	//b=5;
+	//cout<<a+b;
+	
+	return 0;
+}
+ 
+
+
