2a189f1613645629e0289df2e63f1c3f35fb2da0
conghs <conghs@ieslab.cn>
2018-10-21 22:37:08 +0800

    Hello

---
+++ study1/study1.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : study1.cpp
+// Author      : cwb
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+//	puts("Hello World!!!");
+//	return EXIT_SUCCESS;
+	int a;
+	//cin>>a;
+	cout<<"Hello World!!!";
+	return 0;
+}
