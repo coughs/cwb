c31279d23ac95d7d7c974c0b48553c19bae4fbfd
congwb <congwb@cong.com>
2018-10-21 08:29:15 +0800

    coduck-K1-6-homework-3 文博作业第一版。

--- study1/study1.cpp
+++ study1/study1.cpp
@@ -6,17 +6,25 @@
 // Description : Hello World in C, Ansi-style
 //============================================================================
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
 
 using namespace std;
 
-int main(void) {
-//	puts("Hello World!!!");
-//	return EXIT_SUCCESS;
+it main() {
 	int a;
-	//cin>>a;
-	cout<<"Hello World!!!";
+	cin>>a;
+	if(a<60){
+		cout<<"不及格";
+	}
+	else if(a>=60&&a<70){
+		cout<<"及格";
+	}
+	else if(a>=70&&a<90){
+		cout<<"良好";
+	}
+	else if(a>=90){
+		cout<<"优秀";
+	}
+
 	return 0;
 }
