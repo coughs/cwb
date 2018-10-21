7a52cd6205813dbd728b20d9186d18de9afb0e68
congwb <congwb@cong.com>
2018-10-21 08:34:11 +0800

    coduck-K1-6-homework-3 文博作业第二版。 增加for死循环。

--- study1/study1.cpp
+++ study1/study1.cpp
@@ -12,18 +12,20 @@ using namespace std;
 
 int main() {
 	int a;
-	cin>>a;
-	if(a<60){
-		cout<<"不及格";
-	}
-	else if(a>=60&&a<70){
-		cout<<"及格";
-	}
-	else if(a>=70&&a<90){
-		cout<<"良好";
-	}
-	else if(a>=90){
-		cout<<"优秀";
+	for(;;){
+		cin>>a;
+		if(a<60){
+			cout<<"不及格";
+		}
+		else if(a>=60&&a<70){
+			cout<<"及格";
+		}
+		else if(a>=70&&a<90){
+			cout<<"良好";
+		}
+		else if(a>=90){
+			cout<<"优秀";
+		}
 	}
 
 	return 0;
