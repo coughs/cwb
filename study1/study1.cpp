f596833658d85907f8eda683fb565b2a01d75481
congwb <congwb@cong.com>
2018-10-21 08:41:04 +0800

    coduck-K1-6-homework-3 代码格式。

--- study1/study1.cpp
+++ study1/study1.cpp
@@ -12,19 +12,21 @@ using namespace std;
 
 int main() {
 	int a;
+
 	for(;;){
-		cin>>a;
-		if(a<60){
-			cout<<"不及格";
+		cin >> a;
+
+		if(a < 60){
+			cout << "不及格";
 		}
-		else if(a>=60&&a<70){
-			cout<<"及格";
+		else if((a >= 60) && (a < 70)){
+			cout << "及格";
 		}
-		else if(a>=70&&a<90){
-			cout<<"良好";
+		else if((a >= 70) && (a < 90)){
+			cout << "良好";
 		}
-		else if(a>=90){
-			cout<<"优秀";
+		else if(a >= 90){
+			cout << "优秀";
 		}
 	}
 
