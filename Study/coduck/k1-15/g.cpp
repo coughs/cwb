f269b19a49c451c217b705de169f20465e6879b7
conghs <conghs@ieslab.cn>
2018-11-28 15:46:00 +0800

    数组数量； 下表范围； 下标越界。

--- Study/coduck/k1-15/g.cpp
+++ Study/coduck/k1-15/g.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main () {
 	int n,m,b=0;
 	cin>>n;
-	int a[n-1];
+	int a[n];
 	for(int i=1;i<=n;i++){
 		cin>>a[i-1];
 	}
 	cin>>m;
-	for(int i=0;i<=n;i++){
-		if(a[i-1]==m){
+	for(int i=0;i<n;i++){
+		if(a[i]==m){
 			b++;
 		} 
 	}
